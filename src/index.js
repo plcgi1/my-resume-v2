1c3ea00a174bfdc415072e3acfa3e1b6b3298a14
pl <plcgi1@gmail.com>
2018-10-08 09:25:29 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
-import registerServiceWorker from './registerServiceWorker';
+import {unregister} from './registerServiceWorker';
 
 ReactDOM.render(<div><Routes/></div>, document.getElementById('root'));
 
-registerServiceWorker();
+unregister();
